10db9b746c386272acc9f396f599b0905f4081a7
Himanshu <118209252+psjhimanshu@users.noreply.github.com>
2025-04-02 23:31:27 +0530

    home screen me kuch kam kiya h

--- lib/Database/RecentActivityService.dart
+++ lib/Database/RecentActivityService.dart
@@ -1,11 +1,10 @@
-// lib/database/recent_activities_service.dart
+// lib/Database/RecentActivityService.dart
 import 'package:shared_preferences/shared_preferences.dart';
 
 class RecentActivitiesService {
   static const String _key = 'recent_activities';
   List<Map<String, String>> _recentActivities = [];
 
-  // Load activities from SharedPreferences
   Future<void> loadActivities() async {
     final prefs = await SharedPreferences.getInstance();
     final List<String>? savedActivities = prefs.getStringList(_key);
@@ -21,7 +20,6 @@ class RecentActivitiesService {
     }
   }
 
-  // Save an activity to SharedPreferences
   Future<void> saveActivity(String activity, String type, String duration) async {
     final prefs = await SharedPreferences.getInstance();
     _recentActivities.insert(0, {
@@ -29,7 +27,6 @@ class RecentActivitiesService {
       'type': type,
       'duration': duration,
     });
-    // Limit to 10 recent activities
     if (_recentActivities.length > 10) {
       _recentActivities = _recentActivities.sublist(0, 10);
     }
@@ -39,12 +36,21 @@ class RecentActivitiesService {
     );
   }
 
-  // Get the list of recent activities
+  Future<void> deleteActivity(int index) async {
+    final prefs = await SharedPreferences.getInstance();
+    if (index >= 0 && index < _recentActivities.length) {
+      _recentActivities.removeAt(index);
+      await prefs.setStringList(
+        _key,
+        _recentActivities.map((e) => '${e['activity']}|${e['type']}|${e['duration']}').toList(),
+      );
+    }
+  }
+
   List<Map<String, String>> getActivities() {
     return _recentActivities;
   }
 
-  // Clear all activities
   Future<void> clearActivities() async {
     final prefs = await SharedPreferences.getInstance();
     _recentActivities.clear();
