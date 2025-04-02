de81df58c6c2b991bf500b29463f235157711073
somanirahul <secondarysomani@gmail.com>
2025-04-02 23:12:34 +0530

    update stats screen

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -18,7 +18,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Timer(Duration(seconds: 3),(){
+    Timer(Duration(seconds: 1),(){
       Get.off(()=>MainScreen());
     });
   }
